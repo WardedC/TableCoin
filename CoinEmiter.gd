e2368548e86fe70476558374d53e156e44732db3
Edward Cabrera <93951069+WardedC@users.noreply.github.com>
2025-08-20 09:56:44 -0400

    Implemented shadow on coin sprite and little light on mesa

--- CoinEmiter.gd
+++ CoinEmiter.gd
@@ -6,3 +6,4 @@ func _input_event(_vp, event, _shape_idx):
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
 		emit_signal("collected", value)
 		queue_free()
+		
